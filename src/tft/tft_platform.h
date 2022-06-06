7dc4435ddd1bf6b811422a5b00ccda915c04627d
Herb01 <herb1@ziggo.nl>
2022-06-06 10:11:19 +0200

    platform tft changed
    the origional files contaones a link to the files in platform/

--- src/tft/tft_platform.h
+++ src/tft/tft_platform.h
@@ -1,1 +1,21 @@
-platform/tft_opencm3_stm32fx.h
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// speeds up render by avoiding function calls.
+
+#include <libopencm3/stm32/gpio.h>
+
+#define tft_gpio_set(port, pins) GPIO_BSRR(port) = pins
+#define tft_gpio_clear(port, pins) GPIO_BSRR(port) = (pins << 16)
+
+#include "delay.h"
+
+#define tft_delay_microseconds(us) els_delay_microseconds(us)
+
+#ifdef __cplusplus
+}
+#endif
+
